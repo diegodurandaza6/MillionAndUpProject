2bb23483c81cfc6107e23999add0ced34dd1c52a
Diego Duran <diegodurandaza6@gmail.com>
2023-05-28 23:27:44 -0500

    Configuración de test unitarios para el proyecto adapter in

---
+++ Properties.Ms.AdapterInHttp.UnitTest/PropertyControllerUnitTest.cs
@@ -0,0 +1,37 @@
+﻿using Properties.Ms.AdapterInHttp.Controllers.Version1;
+using Moq;
+using Properties.Ms.Domain.Property.IPortsIn;
+using Microsoft.AspNetCore.Mvc;
+using Properties.Ms.Domain.Property.Models;
+
+namespace Properties.Ms.AdapterInHttp.UnitTest
+{
+    internal class PropertyControllerUnitTest
+    {
+        private PropertyController _controller;
+        private Mock<IPropertyService> _requestMock;
+        private PropertyRequest mockRequest;
+        
+        [SetUp]
+        public void Setup()
+        {
+            _requestMock = new Mock<IPropertyService>();
+            _controller = new PropertyController(_requestMock.Object);
+            mockRequest = new() { Name = "Property name 5", Address = "Property address 5", Price = 5000000, Year = 2020, IdOwner = 5 };
+        }
+
+        [Test]
+        public void GetAllPropertiesTestOkResult()
+        {
+            var response = _controller.GetPropertyList(null, null, null);
+            Assert.That(response, Is.InstanceOf<Task<IActionResult>>());
+        }
+
+        [Test]
+        public async Task UpdatePropertyTestOkResult()
+        {
+            var response = await _controller.UpdateProperty(1, mockRequest);
+            Assert.That(response, Is.InstanceOf<OkObjectResult>());
+        }
+    }
+}
