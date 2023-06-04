073e8ee87411d6baf7a25c41dacdc0d67aa84011
Diego Duran <diegodurandaza6@gmail.com>
2023-06-04 15:03:17 -0500

    Eliminación de modelos y constantes migradas al ms de seguridad

--- Properties.Ms.Domain/Property/Models/UserLogin.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Properties.Ms.Domain.Property.Models
-{
-    public class UserLogin
-    {
-        public string UserName { get; set; }
-        public string Password { get; set; }
-    }
-}
